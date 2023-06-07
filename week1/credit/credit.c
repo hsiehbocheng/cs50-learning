50e6861b439c20b7bb1f9988be7090461d7060c6
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-06-07 14:58:23 +0800

    message

--- week1/credit/credit.c
+++ week1/credit/credit.c
@@ -1,7 +1,44 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
+    long number = get_long("Number: ");
+    long output = 0;
+    int i = 1;
+    while (number != 0)
+    {
+        if ( i % 2 == 0)
+        {
+            output += (number % 10) * 2;
+        }else
+        {
+            output += number % 10;
+        }
+        i ++;
+        number /= 10;
+    }
+
+    if (i != 13 && i != 15 && i != 16)
+    {
+        printf("INVALID\n");
+        return 0;
+    }
+
+    long start = i;
+    do
+    {
+        start = start / 10;
+    }
+    while (start > 100);
+
     
+
+    if (output % 10 == 0)
+    {
+        printf("VISA\n");
+    }else
+    {
+        printf("INVALID\n");
+    }
 }
