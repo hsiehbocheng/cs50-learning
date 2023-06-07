f34556a436752afb475d4f115bafa6a8585f1d96
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-06-07 15:45:42 +0800

    add note

--- week1/credit/credit.c
+++ week1/credit/credit.c
@@ -3,17 +3,17 @@
 
 int main(void)
 {
-    long number = get_long("Number: ");
-    long output = 0;
-    int start;
-    int dig;
+    long number = get_long("Number: "); 
+    long output = 0; // 相加的結果
+    int start; // 前兩位數
+    int dig; // 用來加法的暫時變數
     int i = 1;
     while (number != 0)
     {
         if ( i % 2 == 0)
         {
             dig = (number % 10) * 2;
-            if (dig > 9)
+            if (dig > 9) // 規定是將乘完的數字各別相加，所以要判斷當兩位數時要分開相加
             {
                 output += dig % 10;
                 output += dig / 10;
@@ -33,7 +33,7 @@ int main(void)
             start = number;
         }
     }
-    i -= 1;
+    i -= 1; // 上面 loop 是讓 i 從 1 開始，所以要減回來
 
     if (i != 13 && i != 15 && i != 16)
     {
