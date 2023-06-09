fae280b342cc301430bc1e3187302325d791ec22
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-06-09 17:30:15 +0800

    pyclass

--- pyClass/student.py
+++ pyClass/student.py
@@ -1,12 +1,29 @@
+# def main():
+#     name, house = get_student()
+#     print(f"Hello, {name} from {house}!")
+
+# def get_student():
+#     name = input("Name: ")
+#     house = input("House: ")
+#     return name, house
+
+# if __name__ == "__main__":
+#     main()
+
 def main():
-    name, house = get_student()
-    print(f"Hello, {name} from {house}!")
+    student = get_student()
+    print(f"{student.name} from {student.house}!")
+
+class Student:
+    def __init__(self, name="", house=""):
+        self.name = name
+        self.house = house
 
 def get_student():
-    name = input("Name: ")
-    house = input("House: ")
-    return name, house 
+    student = Student()
+    student.name = input("Name: ")
+    student.house = input("House: ")
+    return student
 
 if __name__ == "__main__":
     main()
-
