77299c30438b823f76bdaeabbdd5a9b7d3e7e1fc
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-06-15 16:17:59 +0800

    some

---
+++ pyClass/bank.py
@@ -0,0 +1,15 @@
+class Account:
+    def __init__(self):
+       self._balance = 0
+
+    @property
+    def balance(self):
+        return self._balance
+
+    def deposit(self, n):
+        self._balance += n 
+        
+    def withdraw(self, n):
+        self._balance -= n
+
+     
