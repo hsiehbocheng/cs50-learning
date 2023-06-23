ab0ba3b5cfc62b922056e2884a0600369dc9417f
謝博丞 <96316508+hsiehbocheng@users.noreply.github.com>
2023-06-24 02:19:25 +0800

    some

--- CS50's introduction to Python/hw/shirtificate/shirtificate.py
+++ CS50's introduction to Python/hw/shirtificate/shirtificate.py
@@ -1,5 +1,10 @@
 from fpdf import FPDF
 
+'''
+:ref https://youtu.be/CASv-fUUqcA
+:ref https://cs50.harvard.edu/python/2022/psets/8/shirtificate/#cs50-shirtificate
+'''
+
 name = input("Name: ")
 pdf = FPDF()
 pdf.add_page()
